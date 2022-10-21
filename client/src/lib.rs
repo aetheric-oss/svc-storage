7198591255ec65fa8553072e51481f500ab44f7b
Owlot <95369289+owlot@users.noreply.github.com>
2022-10-21 11:37:54 +0200

    chore(client): rename client to client-grpc

--- client/src/lib.rs
+++
@@ -1,7 +0,0 @@
-//! gRPC client library exposing svc_storage module
-
-///module svc_storage generated from svc-storage.proto
-pub mod svc_storage {
-    #![allow(unused_qualifications, missing_docs)]
-    include!("svc_storage.rs");
-}
