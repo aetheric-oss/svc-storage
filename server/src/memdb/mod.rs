2960fdeb37994374887f044e2e49186da2aab142
owlot <95369289+owlot@users.noreply.github.com>
2023-01-25 18:21:36 +0100

    refactor(vertiport): use generic structs and traits
    BREAKING

--- server/src/memdb/mod.rs
+++ server/src/memdb/mod.rs
@@ -2,11 +2,11 @@
 pub mod macros;
 
 pub use crate::common::MEMDB_LOG_TARGET;
-pub use crate::resources::flight_plan::*;
+pub use crate::resources::flight_plan;
 pub use crate::resources::pilot::*;
 pub use crate::resources::vehicle::*;
 pub use crate::resources::vertipad::*;
-pub use crate::resources::vertiport::*;
+pub use crate::resources::vertiport;
 
 use futures::lock::Mutex;
 use lazy_static::lazy_static;
@@ -14,8 +14,9 @@ use std::collections::HashMap;
 
 lazy_static! {
     pub static ref VEHICLES: Mutex<HashMap<String, Vehicle>> = Mutex::new(HashMap::new());
-    pub static ref VERTIPORTS: Mutex<HashMap<String, Vertiport>> = Mutex::new(HashMap::new());
+    pub static ref VERTIPORTS: Mutex<HashMap<String, vertiport::Data>> = Mutex::new(HashMap::new());
     pub static ref VERTIPADS: Mutex<HashMap<String, Vertipad>> = Mutex::new(HashMap::new());
     pub static ref PILOTS: Mutex<HashMap<String, Pilot>> = Mutex::new(HashMap::new());
-    pub static ref FLIGHT_PLANS: Mutex<HashMap<String, FlightPlan>> = Mutex::new(HashMap::new());
+    pub static ref FLIGHT_PLANS: Mutex<HashMap<String, flight_plan::Data>> =
+        Mutex::new(HashMap::new());
 }
