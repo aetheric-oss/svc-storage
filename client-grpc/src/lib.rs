f06926c0a1f381d6f2d2723669286d8e849490ca
owlot <95369289+owlot@users.noreply.github.com>
2023-01-25 18:21:36 +0100

    feat(vertiport): use generic structs and traits
    BREAKING

--- client-grpc/src/lib.rs
+++ client-grpc/src/lib.rs
@@ -4,11 +4,20 @@
 pub mod client {
     #![allow(unused_qualifications, missing_docs)]
     include!("grpc.rs");
-    include!("grpc.flight_plan.rs");
     include!("grpc.pilot.rs");
     include!("grpc.vehicle.rs");
-    include!("grpc.vertiport.rs");
     include!("grpc.vertipad.rs");
+}
+pub mod flight_plan {
+    #![allow(unused_qualifications, missing_docs)]
+    include!("grpc.flight_plan.rs");
+}
+pub mod vertiport {
+    #![allow(unused_qualifications, missing_docs)]
+    include!("grpc.vertiport.rs");
 }
 
 use crate::client::{Id, SearchFilter, ValidationResult};
+
+pub use flight_plan::rpc_service_client::RpcServiceClient as FlightPlanClient;
+pub use vertiport::rpc_service_client::RpcServiceClient as VertiportClient;
