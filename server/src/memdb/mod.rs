1556cc9f8710d79a6d3c387cff54818dc9f9eb64
owlot <95369289+owlot@users.noreply.github.com>
2023-01-26 16:15:15 +0100

    feat(vertipad): use generic structs and traits
    Closes #30

--- server/src/memdb/mod.rs
+++ server/src/memdb/mod.rs
@@ -1,11 +1,10 @@
 #[macro_use]
 pub mod macros;
 
-pub use crate::common::MEMDB_LOG_TARGET;
 pub use crate::resources::flight_plan;
 pub use crate::resources::pilot::*;
 pub use crate::resources::vehicle::*;
-pub use crate::resources::vertipad::*;
+pub use crate::resources::vertipad;
 pub use crate::resources::vertiport;
 
 use futures::lock::Mutex;
@@ -15,7 +14,7 @@ use std::collections::HashMap;
 lazy_static! {
     pub static ref VEHICLES: Mutex<HashMap<String, Vehicle>> = Mutex::new(HashMap::new());
     pub static ref VERTIPORTS: Mutex<HashMap<String, vertiport::Data>> = Mutex::new(HashMap::new());
-    pub static ref VERTIPADS: Mutex<HashMap<String, Vertipad>> = Mutex::new(HashMap::new());
+    pub static ref VERTIPADS: Mutex<HashMap<String, vertipad::Data>> = Mutex::new(HashMap::new());
     pub static ref PILOTS: Mutex<HashMap<String, Pilot>> = Mutex::new(HashMap::new());
     pub static ref FLIGHT_PLANS: Mutex<HashMap<String, flight_plan::Data>> =
         Mutex::new(HashMap::new());
