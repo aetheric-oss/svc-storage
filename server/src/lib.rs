18ce677cd5972daa6abd3a20bd007cb8131cb098
owlot <95369289+owlot@users.noreply.github.com>
2023-01-30 19:02:44 +0100

    docs(rust): improve server rust-doc

---
+++ server/src/lib.rs
@@ -0,0 +1,12 @@
+#![doc = include_str!("../README.md")]
+
+/// common functions
+pub mod common;
+/// grpc implementation module
+pub mod grpc;
+/// memdb implementation module
+pub mod memdb;
+/// postgres implementation module
+pub mod postgres;
+/// resource implementation module
+pub mod resources;
--- server/src/resources/base/mod.rs
+++ server/src/resources/base/mod.rs
@@ -13,6 +13,7 @@ use crate::common::ArrErr;
 use crate::grpc::{GrpcDataObjectType, Id, ValidationError, ValidationResult};
 use crate::postgres::{PsqlJsonValue, PsqlObjectType, PsqlResourceType};
 
+/// Generic trait providing useful functions for our resources
 pub trait Resource {
     /// Allows us to implement the resource definition used for simple insert and update queries
     fn get_definition() -> ResourceDefinition;
@@ -28,20 +29,26 @@ pub trait Resource {
     }
 }
 
+/// Allows us to transform the gRPC `Object` structs into a generic object
 pub trait GenericObjectType<T>
 where
     Self: PsqlResourceType + Resource,
     T: GrpcDataObjectType,
 {
+    /// Get `Object` struct `id` field, to be overwritten by trait implementor
     fn get_id(&self) -> Option<String> {
         None
     }
+    /// Get `Object` struct `data` field, to be overwritten by trait implementor
     fn get_data(&self) -> Option<T> {
         None
     }
+    /// Set `Object` struct `id` field, to be overwritten by trait implementor
     fn set_id(&mut self, id: String);
+    /// Set `Object` struct `data` field, to be overwritten by trait implementor
     fn set_data(&mut self, data: T);
 
+    /// Get `Object` `id` if set, returns [ArrErr] if no `id` is set
     fn try_get_id(&self) -> Result<String, ArrErr> {
         match self.get_id() {
             Some(id) => Ok(id),
@@ -54,9 +61,11 @@ where
         }
     }
 
+    /// Get `Object` `id` as [Uuid] if the `id` is set and is a valid UUID string, returns [ArrErr] if not
     fn try_get_uuid(&self) -> Result<Uuid, ArrErr> {
         Uuid::parse_str(&self.try_get_id()?).map_err(ArrErr::from)
     }
+    /// Get `Object` `data` if set, returns [ArrErr] if no `data` is set
     fn try_get_data(&self) -> Result<T, ArrErr> {
         match self.get_data() {
             Some(data) => Ok(data),
@@ -71,20 +80,28 @@ where
 }
 
 #[derive(Clone, Debug)]
+/// struct object defining resource metadata
 pub struct ResourceDefinition {
+    /// psql table corresponding to the resource
     pub psql_table: String,
+    /// psql column name used to identify the unique resource in the database
     pub psql_id_col: String,
+    /// resource fields definition
     pub fields: HashMap<String, FieldDefinition>,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
+/// Generic resource wrapper struct used to implement our generic traits
 pub struct GenericResource<T>
 where
     Self: GenericObjectType<T>,
     T: GrpcDataObjectType + prost::Message,
 {
+    /// resource unique id in UUID format
     pub id: Option<String>,
+    /// resource field data
     pub data: Option<T>,
+    /// field mask used for update actions
     pub mask: Option<::prost_types::FieldMask>,
 }
 impl<T: GrpcDataObjectType> PsqlObjectType<T> for GenericResource<T> where Self: GenericObjectType<T>
@@ -108,24 +125,36 @@ where
     }
 }
 
+/// Generic resource result wrapper struct used to implement our generic traits
+#[derive(Debug)]
 pub struct GenericResourceResult<T, U>
 where
     T: GenericObjectType<U>,
     U: GrpcDataObjectType,
 {
+    /// [PhantomData] needed to provide the [GrpcDataObjectType] during implementation
     pub phantom: PhantomData<U>,
+    /// [GenericObjectType] with resource id and data
     pub resource: Option<T>,
+    /// [ValidationResult] returned from the update action
     pub validation_result: ValidationResult,
 }
 
 #[derive(Clone, Debug)]
+/// Field definition struct defining field properties
 pub struct FieldDefinition {
+    /// [PsqlFieldType]
     pub field_type: PsqlFieldType,
+    /// [bool] to set if field is mandatory in the database
     mandatory: bool,
+    /// [bool] to set if field should not be exposed to gRPC object
     internal: bool,
+    /// [String] option to provide a default value used during database inserts
     default: Option<String>,
 }
+
 impl FieldDefinition {
+    /// Create a new [FieldDefinition] with provided field_type and mandatory setting
     pub fn new(field_type: PsqlFieldType, mandatory: bool) -> Self {
         Self {
             field_type,
@@ -134,6 +163,7 @@ impl FieldDefinition {
             default: None,
         }
     }
+    /// Create a new internal [FieldDefinition] with provided field_type and mandatory setting
     pub fn new_internal(field_type: PsqlFieldType, mandatory: bool) -> Self {
         Self {
             field_type,
@@ -143,19 +173,28 @@ impl FieldDefinition {
         }
     }
 
+    /// Returns [bool] mandatory
     pub fn is_mandatory(&self) -> bool {
         self.mandatory
     }
+    /// Returns [bool] internal
     pub fn is_internal(&self) -> bool {
         self.internal
     }
+    /// Returns [bool] `true` if a `default` value has been provided for this field and `false`if not
     pub fn has_default(&self) -> bool {
         self.default.is_some()
     }
+    /// Sets the `default` value using the given default [String]
     pub fn set_default(&mut self, default: String) -> Self {
         self.default = Some(default);
         self.clone()
     }
+    /// Gets the `default` value for this field
+    ///
+    /// The function will panic if no default has been set. It's recommended to call
+    /// [has_default](FieldDefinition::has_default) first, to determine if this function can be used or
+    /// not
     pub fn get_default(&self) -> String {
         if self.has_default() {
             self.default.clone().unwrap()
@@ -278,7 +317,7 @@ pub fn validate_enum(
 macro_rules! build_grpc_resource_impl {
     ($resource:tt) => {
         ///Implementation of gRPC endpoints
-        #[derive(Clone, Default, Debug)]
+        #[derive(Clone, Default, Debug, Copy)]
         pub struct GrpcServer {}
 
         impl GrpcObjectType<GenericResource<Data>, Data> for GrpcServer {}
@@ -306,9 +345,37 @@ macro_rules! build_grpc_resource_impl {
 /// Generates gRPC server generic function implementations
 #[macro_export]
 macro_rules! build_grpc_server_generic_impl {
-    () => {
+    ($rpc_service:expr) => {
         #[tonic::async_trait]
         impl RpcService for GrpcServer {
+            #[doc = concat!("Returns a [tonic] gRCP [Response] containing an ", stringify!($rpc_service), " [Object]")]
+            ///
+            /// # Errors
+            ///
+            /// Returns [`tonic::Status`] with [`tonic::Code::NotFound`] if no record is returned from the database
+            ///
+            /// # Examples
+            ///
+            /// ```
+            /// use svc_storage::grpc::Id;
+            #[doc = concat!("use svc_storage::resources::", stringify!($rpc_service), "::{Object, GrpcServer, RpcService};")]
+            ///
+            /// async fn example() -> Result<Object, tonic::Status> {
+            ///     let server = GrpcServer::default();
+            ///
+            ///     let id = "53acfe06-dd9b-42e8-8cb4-12a2fb2fa693".to_string();
+            ///     let result = match server.get_by_id(tonic::Request::new(Id { id })).await
+            ///     {
+            ///         Ok(res) => res.into_inner(),
+            ///         Err(e) => {
+            ///             return Err(e);
+            ///         },
+            ///     };
+            ///     log::debug!("{:?}", result);
+            ///
+            ///     Ok(result)
+            /// }
+            /// ```
             async fn get_by_id(
                 &self,
                 request: Request<Id>,
@@ -316,6 +383,40 @@ macro_rules! build_grpc_server_generic_impl {
                 self.generic_get_by_id(request).await
             }
 
+            /// Takes a [`SearchFilter`] object to search the database with the provided values.
+            ///
+            /// This method supports paged results.
+            /// When the `search_field` and `search_value` are empty, no filters will be applied.
+            ///
+            /// # Examples
+            ///
+            /// ```
+            /// use svc_storage::grpc::{SearchFilter, Id};
+            #[doc = concat!("use svc_storage::resources::", stringify!($rpc_service), "::{Object, List, GrpcServer, RpcService};")]
+            ///
+            /// async fn example() -> Result<(), tonic::Status> {
+            ///     let server = GrpcServer::default();
+            ///
+            ///     // Empty filter, but return paged results
+            ///     let mut filter = SearchFilter {
+            ///         search_field: String::from(""),
+            ///         search_value: String::from(""),
+            ///         page_number: 1,
+            ///         results_per_page: 50,
+            ///     };
+            ///
+            ///     let result = match server.get_all_with_filter(tonic::Request::new(filter.clone())).await
+            ///     {
+            ///         Ok(res) => res.into_inner().list,
+            ///         Err(e) => {
+            ///             return Err(e);
+            ///         },
+            ///     };
+            ///     log::debug!("{:?}", result);
+            ///
+            ///     Ok(())
+            /// }
+            /// ```
             async fn get_all_with_filter(
                 &self,
                 request: Request<SearchFilter>,
@@ -323,6 +424,37 @@ macro_rules! build_grpc_server_generic_impl {
                 self.generic_get_all_with_filter::<List>(request).await
             }
 
+            #[doc = concat!("Takes a ", stringify!($rpc_service), " [Data] object to create a new ", stringify!($rpc_service), " with the provided data.")]
+            ///
+            /// A new UUID will be generated by the database and returned as `id` as part of the returned [Object].
+            ///
+            /// # Example
+            /// ```
+            /// use svc_storage::grpc::Id;
+            #[doc = concat!("use svc_storage::resources::", stringify!($rpc_service), "::{Data, Response, GrpcServer, RpcService};")]
+            #[doc = concat!("use svc_storage::resources::", stringify!($rpc_service), "::mock;")]
+            ///
+            /// const CAL_WORKDAYS_7AM_6PM: &str = "\
+            /// DTSTART:20221019T180000Z;DURATION:PT14H
+            /// RRULE:FREQ=WEEKLY;BYDAY=MO,TU,WE,TH,FR
+            /// DTSTART:20221021T000000Z;DURATION:PT24H
+            /// RRULE:FREQ=WEEKLY;BYDAY=SA,SU";
+            ///
+            /// async fn example() -> Result<(), tonic::Status> {
+            ///     let server = GrpcServer::default();
+            ///
+            ///     let result = match server.insert(tonic::Request::new(mock::get_data_obj())).await
+            ///     {
+            ///         Ok(res) => res.into_inner(),
+            ///         Err(e) => {
+            ///             return Err(e);
+            ///         },
+            ///     };
+            ///     log::debug!("{:?}", result);
+            ///
+            ///     Ok(())
+            /// }
+            /// ```
             async fn insert(
                 &self,
                 request: Request<Data>,
@@ -330,6 +462,36 @@ macro_rules! build_grpc_server_generic_impl {
                 self.generic_insert::<Response>(request).await
             }
 
+            #[doc = concat!("Takes a ", stringify!($rpc_service), " [UpdateObject] to new data to the database")]
+            ///
+            /// A field mask can be provided to restrict updates to specific fields.
+            /// Returns the updated [Response] on success.
+            ///
+            /// # Example
+            /// ```
+            /// use svc_storage::grpc::Id;
+            #[doc = concat!("use svc_storage::resources::", stringify!($rpc_service), "::{UpdateObject, Response, GrpcServer, RpcService};")]
+            #[doc = concat!("use svc_storage::resources::", stringify!($rpc_service), "::mock;")]
+            ///
+            /// async fn example() -> Result<(), tonic::Status> {
+            ///     let server = GrpcServer::default();
+            ///
+            ///     let result = match server.update(tonic::Request::new(UpdateObject {
+            ///         id: "54acfe06-dd9b-42e8-8cb4-12a2fb2fa693".to_string(),
+            ///         data: Some(mock::get_data_obj()),
+            ///         mask: None
+            ///     })).await
+            ///     {
+            ///         Ok(res) => res.into_inner(),
+            ///         Err(e) => {
+            ///             return Err(e);
+            ///         },
+            ///     };
+            ///     log::debug!("{:?}", result);
+            ///
+            ///     Ok(())
+            /// }
+            /// ```
             async fn update(
                 &self,
                 request: Request<UpdateObject>,
@@ -337,6 +499,29 @@ macro_rules! build_grpc_server_generic_impl {
                 self.generic_update::<Response, UpdateObject>(request).await
             }
 
+            #[doc = concat!("Takes an [Id] to set the matching ", stringify!($rpc_service), " record as deleted in the database")]
+            ///
+            /// # Example
+            /// ```
+            /// use svc_storage::grpc::Id;
+            #[doc = concat!("use svc_storage::resources::", stringify!($rpc_service), "::{GrpcServer, RpcService};")]
+            ///
+            /// async fn example() -> Result<(), tonic::Status> {
+            ///     let server = GrpcServer::default();
+            ///
+            ///     let id = "53acfe06-dd9b-42e8-8cb4-12a2fb2fa693".to_string();
+            ///     let result = match server.delete(tonic::Request::new(Id { id })).await
+            ///     {
+            ///         Ok(res) => res.into_inner(),
+            ///         Err(e) => {
+            ///             return Err(e);
+            ///         },
+            ///     };
+            ///     log::debug!("{:?}", result);
+            ///
+            ///     Ok(())
+            /// }
+            /// ```
             async fn delete(&self, request: Request<Id>) -> Result<tonic::Response<()>, Status> {
                 self.generic_delete(request).await
             }
