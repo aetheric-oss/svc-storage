e993c77e6245239def155d3f63ed78b32c68873a
owlot <95369289+owlot@users.noreply.github.com>
2023-02-06 11:34:33 +0100

    docs(rust): improve grpc client rust-doc

--- client-grpc/src/grpc.vertiport.rs
+++ client-grpc/src/grpc.vertiport.rs
@@ -1,11 +1,17 @@
+/// Response struct returning an \[Object\] on success and \[ValidationResult\] if invalid fields were provided
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Response {
+    /// struct with field -> error pairs to provide feedback about invalid fields
     #[prost(message, optional, tag = "1")]
     pub validation_result: ::core::option::Option<super::ValidationResult>,
+    /// Object struct with id \[String\] in \[Uuid\](uuid::Uuid) format and \[Data\] struct with vertiport data
     #[prost(message, optional, tag = "2")]
     pub object: ::core::option::Option<Object>,
 }
+/// Object struct with `id` and `data` field
+/// * `id` \[String\] in \[Uuid\](uuid::Uuid) format
+/// * `data` \[Data\] struct with vertiport data
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Object {
@@ -16,36 +22,48 @@ pub struct Object {
     #[prost(message, optional, tag = "2")]
     pub data: ::core::option::Option<Data>,
 }
+/// UpdateObject struct with `id`, `data` and `mask` fields
+/// * `id` \[String\] in \[Uuid\](uuid::Uuid) format
+/// * `data` \[Data\] struct with vertiport data which should be used for update
+/// * `mask` \[FieldMask\] struct with vertiport fields that should be updated
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct UpdateObject {
-    /// id UUID v4
+    /// `id` \[String\] in \[Uuid\](uuid::Uuid) format
     #[prost(string, tag = "1")]
     pub id: ::prost::alloc::string::String,
+    /// struct with vertiport data which should be used for update
     #[prost(message, optional, tag = "2")]
     pub data: ::core::option::Option<Data>,
+    /// struct with vertiport fields that should be updated
     #[prost(message, optional, tag = "3")]
     pub mask: ::core::option::Option<::prost_types::FieldMask>,
 }
+/// Data struct with vertiport data
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Data {
+    /// human readable name of the vertiport
     #[prost(string, tag = "1")]
     pub name: ::prost::alloc::string::String,
+    /// additional description of the vertiport
     #[prost(string, tag = "2")]
     pub description: ::prost::alloc::string::String,
+    /// vertipad latitude location
     #[prost(double, tag = "3")]
     pub latitude: f64,
+    /// vertipad longitude location
     #[prost(double, tag = "4")]
     pub longitude: f64,
-    /// repeated string engineers = 6;
-    /// uint32 elevation = 7;
+    /// RRULE data string to indicate the vertipad's opening days and hours
     #[prost(string, optional, tag = "5")]
     pub schedule: ::core::option::Option<::prost::alloc::string::String>,
 }
+/// Struct containing a `list` of vertiport \[Vec\<Object\>\]
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct List {
+    /// array/vector of vertiport items
     #[prost(message, repeated, tag = "1")]
     pub list: ::prost::alloc::vec::Vec<Object>,
 }
@@ -119,6 +137,8 @@ pub mod rpc_service_client {
             self.inner = self.inner.accept_compressed(encoding);
             self
         }
+        /// Search vertiports using a simple filter
+        /// This function will be deprecated soon, please use `search` instead
         pub async fn get_all_with_filter(
             &mut self,
             request: impl tonic::IntoRequest<super::super::SearchFilter>,
@@ -138,6 +158,34 @@ pub mod rpc_service_client {
             );
             self.inner.unary(request.into_request(), path, codec).await
         }
+        /// Returns a [`tonic::Response`] containing an vertiport [`Object`](super::Object)
+        /// Takes an [`id`](super::super::Id) to find the right record to return.
+        ///
+        /// # Errors
+        ///
+        /// Returns [`tonic::Status`] with [`Code::NotFound`](tonic::Code::NotFound) if no record is returned from the database
+        ///
+        /// # Examples
+        /// ```
+        /// use svc_storage_client_grpc::client::Id;
+        /// use svc_storage_client_grpc::VertiportClient;
+        ///
+        /// async fn example () -> Result<(), Box<dyn std::error::Error>> {
+        ///     let mut vertiport_client = VertiportClient::connect("http://localhost:50051").await?;
+        ///
+        ///     let id = "53acfe06-dd9b-42e8-8cb4-12a2fb2fa693".to_owned();
+        ///     match vertiport_client
+        ///         .get_by_id(tonic::Request::new(Id { id }))
+        ///         .await
+        ///     {
+        ///         Ok(res) => {
+        ///           println!("RESPONSE Vertiport By ID={:?}", res);
+        ///           Ok(())
+        ///         },
+        ///         Err(e) => Err(Box::new(e))
+        ///     }
+        /// }
+        /// ```
         pub async fn get_by_id(
             &mut self,
             request: impl tonic::IntoRequest<super::super::Id>,
@@ -157,6 +205,54 @@ pub mod rpc_service_client {
             );
             self.inner.unary(request.into_request(), path, codec).await
         }
+        /// Returns a [`tonic::Response`] containing a vertiport [`Response`](super::Response) object
+        /// of the inserted record after saving the provided vertiport [`Data`](super::Data)
+        ///
+        /// The given data will be validated before insert.
+        /// A new UUID will be generated by the database and returned as `id` as part of the returned vertiport [`Response`](super::Response).
+        /// Any errors found during validation will be added to the [`ValidationResult`](super::super::ValidationResult).
+        ///
+        /// # Errors
+        ///
+        /// Returns [`Status`](tonic::Status) with [`Code::Internal`](tonic::Code::Internal) if the [`tonic::Request`] doesn't contain any data.
+        /// Returns [`Status`](tonic::Status) with [`Code::Internal`](tonic::Code::Internal) if any error is returned from a db call.
+        ///
+        /// # Examples
+        /// ```
+        /// use svc_storage_client_grpc::client::Id;
+        /// use svc_storage_client_grpc::VertiportClient;
+        /// use svc_storage_client_grpc::vertiport::Data;
+        ///
+        /// const CAL_WORKDAYS_8AM_6PM: &str = "\
+        /// DTSTART:20221020T180000Z;DURATION:PT14H
+        /// RRULE:FREQ=WEEKLY;BYDAY=MO,TU,WE,TH,FR
+        /// DTSTART:20221022T000000Z;DURATION:PT24H
+        /// RRULE:FREQ=WEEKLY;BYDAY=SA,SU";
+        ///
+        /// async fn example () -> Result<(), Box<dyn std::error::Error>> {
+        ///     let mut vertiport_client = VertiportClient::connect("http://localhost:50051").await?;
+        ///
+        ///     let departure_vertipad_id = "53acfe06-dd9b-42e8-8cb4-12a2fb2fa693".to_owned();
+        ///     let destination_vertipad_id = "db67da52-2280-4316-8b29-9cf1bff65931".to_owned();
+        ///     println!("Starting insert vertiport");
+        ///     match vertiport_client
+        ///     .insert(tonic::Request::new(Data {
+        ///         name: "My favorite port".to_owned(),
+        ///         description: "Open during workdays and work hours only".to_owned(),
+        ///         latitude: -122.4194,
+        ///         longitude: 37.7746,
+        ///         schedule: Some(CAL_WORKDAYS_8AM_6PM.to_owned()),
+        ///     }))
+        ///     .await
+        ///     {
+        ///         Ok(res) => {
+        ///           println!("RESPONSE Vertiport Insert={:?}", res);
+        ///           Ok(())
+        ///         },
+        ///         Err(e) => Err(Box::new(e))
+        ///     }
+        /// }
+        /// ```
         pub async fn insert(
             &mut self,
             request: impl tonic::IntoRequest<super::Data>,
@@ -176,6 +272,64 @@ pub mod rpc_service_client {
             );
             self.inner.unary(request.into_request(), path, codec).await
         }
+        /// Returns a [`tonic::Response`] containing a vertiport [`Response`](super::Response) object
+        /// of the updated record after saving the provided vertiport [`Data`](super::Data)
+        ///
+        /// The given data will be validated before insert.
+        /// Any errors found during validation will be added to the [`ValidationResult`](super::super::ValidationResult).
+        /// A field [`prost_types::FieldMask`] can be provided to restrict updates to specific fields.
+        ///
+        /// # Errors
+        ///
+        /// Returns [`Status`](tonic::Status) with [`Code::Cancelled`](tonic::Code::Cancelled) if the [`Request`](tonic::Request) doesn't contain any data.
+        /// Returns [`Status`](tonic::Status) with [`Code::Internal`](tonic::Code::Internal) if any error is returned from a db call.
+        /// Returns [`Status`](tonic::Status) with [`Code::Internal`](tonic::Code::Internal) if the provided Id can not be converted to a [`uuid::Uuid`].
+        /// Returns [`Status`](tonic::Status) with [`Code::Internal`](tonic::Code::Internal) if the resulting Vec<tokio_postgres::Row> data could not be converted into [`List`](super::List).
+        ///
+        /// # Examples
+        /// ```
+        /// use svc_storage_client_grpc::client::{ Id };
+        /// use svc_storage_client_grpc::FieldMask;
+        /// use svc_storage_client_grpc::VertiportClient;
+        /// use svc_storage_client_grpc::vertiport::{Data, UpdateObject};
+        ///
+        /// async fn example () -> Result<(), Box<dyn std::error::Error>> {
+        ///     let mut vertiport_client = VertiportClient::connect("http://localhost:50051").await?;
+        ///
+        ///     let id = "53acfe06-dd9b-42e8-8cb4-12a2fb2fa693".to_owned();
+        ///     let response = match vertiport_client
+        ///         .get_by_id(tonic::Request::new(Id { id: id.clone() }))
+        ///         .await
+        ///     {
+        ///         Ok(res) => {
+        ///           println!("RESPONSE Vertiport By ID={:?}", res);
+        ///           res
+        ///         },
+        ///         Err(e) => {
+        ///             return Err(Box::new(e));
+        ///         }
+        ///     };
+        ///
+        ///     let vertiport = response.into_inner().data.unwrap();
+        ///     match vertiport_client.update(tonic::Request::new(UpdateObject {
+        ///         id,
+        ///         data: Some(Data {
+        ///             name: "My less favorite port".to_owned(),
+        ///             ..vertiport
+        ///         }),
+        ///         mask: Some(FieldMask {
+        ///             paths: vec!["data.name".to_owned()],
+        ///         }),
+        ///     })).await
+        ///     {
+        ///         Ok(res) => {
+        ///           println!("RESPONSE Vertiport Update={:?}", res);
+        ///           Ok(())
+        ///         },
+        ///         Err(e) => Err(Box::new(e))
+        ///     }
+        /// }
+        /// ```
         pub async fn update(
             &mut self,
             request: impl tonic::IntoRequest<super::UpdateObject>,
@@ -195,6 +349,32 @@ pub mod rpc_service_client {
             );
             self.inner.unary(request.into_request(), path, codec).await
         }
+        /// Takes an [`Id`](super::super::Id) to set the matching vertiport record as deleted in the database"
+        ///
+        /// # Errors
+        ///
+        /// Returns [`Status`](tonic::Status) with [`Code::NotFound`](tonic::Code::NotFound) if no record is returned from the database.
+        /// Returns [`Status`](tonic::Status) with [`Code::Internal`](tonic::Code::Internal) if any error is returned from a db call.
+        ///
+        /// # Examples
+        /// ```
+        /// use svc_storage_client_grpc::client::Id;
+        /// use svc_storage_client_grpc::VertiportClient;
+        ///
+        /// async fn example () -> Result<(), Box<dyn std::error::Error>> {
+        ///     let mut vertiport_client = VertiportClient::connect("http://localhost:50051").await?;
+        ///
+        ///     let id = "53acfe06-dd9b-42e8-8cb4-12a2fb2fa693".to_owned();
+        ///     match vertiport_client.delete(tonic::Request::new(Id{id})).await
+        ///     {
+        ///         Ok(res) => {
+        ///           println!("RESPONSE Vertiport Delete={:?}", res);
+        ///           Ok(())
+        ///         },
+        ///         Err(e) => Err(Box::new(e))
+        ///     }
+        /// }
+        /// ```
         pub async fn delete(
             &mut self,
             request: impl tonic::IntoRequest<super::super::Id>,
@@ -214,6 +394,37 @@ pub mod rpc_service_client {
             );
             self.inner.unary(request.into_request(), path, codec).await
         }
+        /// Search vertiports using an advanced filter
+        ///
+        /// This method supports paged results.
+        ///
+        /// # Errors
+        ///
+        /// Returns [`Status`](tonic::Status) with [`Code::Internal`](tonic::Code::Internal) if any error is returned from the db search result.
+        /// Returns [`Status`](tonic::Status) with [`Code::Internal`](tonic::Code::Internal) if the resulting Vec<tokio_postgres::Row> data could not be converted into [`List`](super::List).
+        ///
+        /// # Examples
+        /// ```
+        /// use svc_storage_client_grpc::VertiportClient;
+        /// use svc_storage_client_grpc::client::AdvancedSearchFilter;
+        ///
+        /// async fn example () -> Result<(), Box<dyn std::error::Error>> {
+        ///     let mut vertiport_client = VertiportClient::connect("http://localhost:50051").await?;
+        ///
+        ///     let filter = AdvancedSearchFilter::search_between("latitude".to_owned(), (-100).to_string(), (-125).to_string());
+        ///
+        ///     match vertiport_client
+        ///         .search(tonic::Request::new(filter))
+        ///         .await
+        ///     {
+        ///         Ok(res) => {
+        ///           println!("RESPONSE Vertiport Search={:?}", res);
+        ///           Ok(())
+        ///         },
+        ///         Err(e) => Err(Box::new(e))
+        ///     }
+        /// }
+        /// ```
         pub async fn search(
             &mut self,
             request: impl tonic::IntoRequest<super::super::AdvancedSearchFilter>,
