dc92044a8a3f15dc7b3ab7b6e070546c95487dbb
owlot <95369289+owlot@users.noreply.github.com>
2023-02-06 11:34:33 +0100

    docs(rust): improve grpc client rust-doc

--- server/src/resources/vertipad/mod.rs
+++ server/src/resources/vertipad/mod.rs
@@ -4,7 +4,7 @@
 pub mod mock;
 
 mod grpc_server {
-    #![allow(unused_qualifications, missing_docs)]
+    #![allow(unused_qualifications)]
     tonic::include_proto!("grpc.vertipad");
 }
 // Expose module resources
