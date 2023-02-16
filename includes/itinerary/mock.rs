3da4dd60e2dbe7ed96651c2ac77dc3d4ddf4cf45
AM Smith <smitham42@gmail.com>
2023-02-15 19:38:41 -0800

    chore: add status field to itinerary

--- includes/itinerary/mock.rs
+++ includes/itinerary/mock.rs
@@ -1,9 +1,10 @@
-use super::Data;
+use super::{Data, ItineraryStatus};
 use uuid::Uuid;
 
 /// Creates a new [Data] object with fields set with random data
 pub fn get_data_obj() -> Data {
     Data {
         user_id: Uuid::new_v4().to_string(),
+        status: ItineraryStatus::Active as i32
     }
 }
