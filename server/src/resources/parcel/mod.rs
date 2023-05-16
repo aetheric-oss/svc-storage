df1eea550e04492e1f99f08d008158ab06159fd7
Xipu Li <lixipu98@gmail.com>
2023-05-16 16:38:07 -0700

    feat: add parcel implementation

--- server/src/resources/parcel/mod.rs
+++ server/src/resources/parcel/mod.rs
@@ -2,10 +2,9 @@
 
 pub use crate::grpc::server::parcel::*;
 
-use chrono::{DateTime, Utc};
-use lib_common::time::datetime_to_timestamp;
 use log::debug;
 use std::collections::HashMap;
+use std::str::FromStr;
 use tokio_postgres::row::Row;
 use tokio_postgres::types::Type as PsqlFieldType;
 use uuid::Uuid;
@@ -13,12 +12,28 @@ use uuid::Uuid;
 use super::base::simple_resource::*;
 use super::base::{FieldDefinition, ResourceDefinition};
 use crate::common::ArrErr;
-use crate::grpc::{GrpcDataObjectType, GrpcField, GrpcFieldOption};
+use crate::grpc::{GrpcDataObjectType, GrpcField};
 
 crate::build_generic_resource_impl_from!();
 
 // Generate grpc server implementations
 crate::build_grpc_simple_resource_impl!(parcel);
+
+impl FromStr for ParcelStatus {
+    type Err = ArrErr;
+
+    fn from_str(s: &str) -> ::core::result::Result<ParcelStatus, Self::Err> {
+        match s {
+            "NOTDROPPEDOF" => ::core::result::Result::Ok(ParcelStatus::Notdroppedof),
+            "DROPPEDOFF" => ::core::result::Result::Ok(ParcelStatus::Droppedoff),
+            "ENROUTE" => ::core::result::Result::Ok(ParcelStatus::Enroute),
+            "ARRIVED" => ::core::result::Result::Ok(ParcelStatus::Arrived),
+            "PICKEDUP" => ::core::result::Result::Ok(ParcelStatus::Pickedup),
+            "COMPLETE" => ::core::result::Result::Ok(ParcelStatus::Complete),
+            _ => ::core::result::Result::Err(ArrErr::Error(format!("Unknown ParcelStatus: {}", s))),
+        }
+    }
+}
 
 impl Resource for ResourceObject<Data> {
     fn get_definition() -> ResourceDefinition {
@@ -28,7 +43,8 @@ impl Resource for ResourceObject<Data> {
             fields: HashMap::from([
                 (
                     "status".to_string(),
-                    FieldDefinition::new(PsqlFieldType::INT2, true),
+                    FieldDefinition::new(PsqlFieldType::ANYENUM, true)
+                        .set_default(String::from("'NOTDROPPEDOFF'")),
                 ),
                 (
                     "itinerary_id".to_string(),
@@ -53,6 +69,14 @@ impl Resource for ResourceObject<Data> {
         }
     }
 
+    /// Converts raw i32 values into string based on matching Enum value
+    fn get_enum_string_val(field: &str, value: i32) -> Option<String> {
+        match field {
+            "status" => ParcelStatus::from_i32(value).map(|val| val.as_str_name().to_string()),
+            _ => None,
+        }
+    }
+
     fn get_table_indices() -> Vec<String> {
         [
             r#"ALTER TABLE parcel ADD CONSTRAINT fk_itinerary_id FOREIGN KEY(itinerary_id) REFERENCES itinerary_flight_plan(itinerary_id)"#.to_owned(),
@@ -64,7 +88,7 @@ impl GrpcDataObjectType for Data {
     fn get_field_value(&self, key: &str) -> Result<GrpcField, ArrErr> {
         match key {
             "itinerary_id" => Ok(GrpcField::String(self.itinerary_id.clone())),
-            "status" => Ok(GrpcField::I16(self.status.clone())),
+            "status" => Ok(GrpcField::I32(self.status)),
             _ => Err(ArrErr::Error(format!(
                 "Invalid key specified [{}], no such field found",
                 key
@@ -79,9 +103,14 @@ impl TryFrom<Row> for Data {
     fn try_from(row: Row) -> Result<Self, ArrErr> {
         debug!("Converting Row to parcel::Data: {:?}", row);
         let itinerary_id: Uuid = row.get("itinerary_id");
+
+        let result = ParcelStatus::from_str(row.get("status"));
+        let Ok(status) = result else {
+            return Err(result.unwrap_err());
+        };
         Ok(Data {
             itinerary_id: itinerary_id.to_string(),
-            status: row.get("status"),
+            status: status.into(),
         })
     }
 }
