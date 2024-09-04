20f5e46fcd9cc5d7b67bbccd5dfb671db2a8815d
owlot <95369289+owlot@users.noreply.github.com>
2024-09-04 18:44:49 +0200

    fix(client-grpc): fix rest references for geo types

--- client-grpc/build.rs
+++ client-grpc/build.rs
@@ -8,5 +8,17 @@ include!("../includes/build.rs");
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     build_proto("../out/grpc/client", false, true)?;
 
+    let cur_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let proto_dir = "../proto";
+
+    let builder = get_grpc_builder_config(&format!("{}/{}", cur_dir, "../out/grpc/client/"));
+    builder
+        .type_attribute("GeoPointZ", "#[derive(ToSchema, IntoParams)]")
+        .type_attribute("GeoPolygonZ", "#[derive(ToSchema, IntoParams)]")
+        .type_attribute("GeoLineStringZ", "#[derive(ToSchema, IntoParams)]")
+        .build_server(false)
+        .build_client(false)
+        .compile(&[get_file(proto_dir, "geo_types".to_owned())], &[proto_dir])?;
+
     Ok(())
 }
